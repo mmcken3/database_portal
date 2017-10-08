8aed9227a03eb087ec3d675ae11e4dc88e5b6200
mmcken3 <mmcken3@g.clemson.edu>
2017-10-08 18:12:46 -0400

    Updated for composite keys and full sample data

--- data/import.sql
+++ data/import.sql
@@ -6,16 +6,19 @@
 -- Create table for 'employees'
 -- ------------------------------------------
 CREATE TABLE IF NOT EXISTS employees (
-	user_name VARCHAR(25) NOT NULL UNIQUE PRIMARY KEY,
-	first_name VARCHAR(25),
-	last_name VARCHAR(25),
-	admin_rights BOOLEAN NOT NULL, 
-	user_pass VARCHAR(25), 
 	user_id INT NOT NULL UNIQUE AUTO_INCREMENT,
-	phone VARCHAR(25),
-	user_address VARCHAR(25),
-	project_id INT,
-	department_id INT
+	user_name VARCHAR(40) NOT NULL UNIQUE,
+	first_name VARCHAR(40),
+	last_name VARCHAR(40),
+	admin_rights BOOLEAN NOT NULL, 
+	user_pass VARCHAR(40), 
+	phone VARCHAR(40),
+	user_address VARCHAR(40),
+	emp_project_id INT,
+	emp_project_name VARCHAR(40),
+	emp_department_id INT,
+	emp_department_name VARCHAR(40),
+	PRIMARY KEY (user_name, user_id)
 );
 
 -- ------------------------------------------
@@ -23,12 +26,14 @@ CREATE TABLE IF NOT EXISTS employees (
 -- ------------------------------------------
 
 CREATE TABLE IF NOT EXISTS suppliers (
-	supplier_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	supplier_name VARCHAR(25),
-	location_id INT NOT NULL,
-	product_type VARCHAR(25),
-	contact_name VARCHAR(25),
-	phone_number VARCHAR(30)
+	supplier_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	supplier_name VARCHAR(40) NOT NULL,
+	supp_location_id INT NOT NULL,
+	supp_location_name VARCHAR(40) NOT NULL,
+	product_type VARCHAR(40),
+	contact_name VARCHAR(40),
+	phone_number VARCHAR(40),
+	PRIMARY KEY (supplier_id, supplier_name)
 );
 
 -- ------------------------------------------
@@ -36,12 +41,15 @@ CREATE TABLE IF NOT EXISTS suppliers (
 -- ------------------------------------------
 
 CREATE TABLE IF NOT EXISTS inventory (
-	inventory_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	inventory_type VARCHAR(30),
-	category VARCHAR(30),
-	supplier_id INT NOT NULL,
-	location_id INT NOT NULL,
-	number_of INT NOT NULL
+	inventory_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	inventory_type VARCHAR(40) NOT NULL,
+	category VARCHAR(40),
+	number_of INT NOT NULL,
+	inv_supplier_id INT NOT NULL,
+	inv_supplier_name VARCHAR(40) NOT NULL,
+	inv_location_name VARCHAR(40) NOT NULL,
+	inv_location_id INT NOT NULL,
+	PRIMARY KEY (inventory_id, inventory_type)
 );
 
 -- ------------------------------------------
@@ -49,13 +57,14 @@ CREATE TABLE IF NOT EXISTS inventory (
 -- ------------------------------------------
 
 CREATE TABLE IF NOT EXISTS locations (
-	location_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	location_name VARCHAR(30),
-	location_type VARCHAR(30),
-	loc_address VARCHAR(30),
-	city VARCHAR(30),
-	loc_state VARCHAR(30),
-	zip INT
+	location_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	location_name VARCHAR(40) NOT NULL,
+	location_type VARCHAR(40),
+	loc_address VARCHAR(40),
+	city VARCHAR(40),
+	loc_state VARCHAR(40),
+	zip INT,
+	PRIMARY KEY (location_id, location_name)
 );
 
 -- ------------------------------------------
@@ -63,11 +72,14 @@ CREATE TABLE IF NOT EXISTS locations (
 -- ------------------------------------------
 
 CREATE TABLE IF NOT EXISTS projects (
-	project_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	project_name VARCHAR(30),
-	customer_id INT,
-	project_type VARCHAR(30),
-	location_id INT
+	project_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	project_name VARCHAR(40) UNIQUE NOT NULL,
+	proj_customer_id INT,
+	proj_customer_name VARCHAR(40),
+	project_type VARCHAR(40),
+	proj_location_id INT NOT NULL,
+	proj_location_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (project_name, project_id)
 );
 
 -- ------------------------------------------
@@ -75,11 +87,13 @@ CREATE TABLE IF NOT EXISTS projects (
 -- ------------------------------------------
 
 CREATE TABLE IF NOT EXISTS customers (
-	customer_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	customer_name VARCHAR(30),
-	contact_name VARCHAR(30),
-	contact_number VARCHAR(30),
-	location_id INT
+	customer_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	customer_name VARCHAR(40) NOT NULL,
+	contact_name VARCHAR(40),
+	contact_number VARCHAR(40),
+	cust_location_id INT,
+	cust_location_name VARCHAR(40),
+	PRIMARY KEY (customer_id, customer_name)
 );
 
 -- ------------------------------------------
@@ -87,20 +101,22 @@ CREATE TABLE IF NOT EXISTS customers (
 -- ------------------------------------------
 
 CREATE TABLE IF NOT EXISTS departments (
-	department_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	department_name VARCHAR(30),
-	dept_manager_id INT
+	department_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	department_name VARCHAR(40),
+	dept_manager_id INT,
+	dept_manager_name VARCHAR(40),
+	PRIMARY KEY (department_id, department_name)
 );
 -- ------------------------------------------
 -- Add foreign key relations
 -- ------------------------------------------
 
-ALTER TABLE employees ADD FOREIGN KEY (project_id) REFERENCES projects(project_id);
-ALTER TABLE employees ADD FOREIGN KEY (department_id) REFERENCES departments(department_id);
-ALTER TABLE suppliers ADD FOREIGN KEY (location_id) REFERENCES locations(location_id);
-ALTER TABLE inventory ADD FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id);
-ALTER TABLE inventory ADD FOREIGN KEY (location_id) REFERENCES locations(location_id);
-ALTER TABLE projects ADD FOREIGN KEY (location_id) REFERENCES locations(location_id);
-ALTER TABLE projects ADD FOREIGN KEY (customer_id) REFERENCES customers(customer_id);
-ALTER TABLE customers ADD FOREIGN KEY (location_id) REFERENCES locations(location_id);
-ALTER TABLE departments ADD FOREIGN KEY (dept_manager_id) REFERENCES employees(user_id);
+ALTER TABLE employees ADD FOREIGN KEY (emp_project_id, emp_project_name) REFERENCES projects(project_id, project_name);
+ALTER TABLE employees ADD FOREIGN KEY (emp_department_id, emp_department_name) REFERENCES departments(department_id, department_name);
+ALTER TABLE suppliers ADD FOREIGN KEY (supp_location_id, supp_location_name) REFERENCES locations(location_id, location_name);
+ALTER TABLE inventory ADD FOREIGN KEY (inv_supplier_id, inv_supplier_name) REFERENCES suppliers(supplier_id, supplier_name);
+ALTER TABLE inventory ADD FOREIGN KEY (inv_location_id, inv_location_name) REFERENCES locations(location_id, location_name);
+ALTER TABLE projects ADD FOREIGN KEY (proj_location_id, proj_location_name) REFERENCES locations(location_id, location_name);
+ALTER TABLE projects ADD FOREIGN KEY (proj_customer_id, proj_customer_name) REFERENCES customers(customer_id, customer_name);
+ALTER TABLE customers ADD FOREIGN KEY (cust_location_id, cust_location_name) REFERENCES locations(location_id, location_name);
+ALTER TABLE departments ADD FOREIGN KEY (dept_manager_id, dept_manager_name) REFERENCES employees(user_id, user_name);
