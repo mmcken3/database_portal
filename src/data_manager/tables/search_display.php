72e3905788b18cbd0ecca6931898c8c31bfc9c7d
mmcken3 <mmcken3@g.clemson.edu>
2017-10-20 09:22:10 -0400

    location search added

--- src/data_manager/tables/search_display.php
+++ src/data_manager/tables/search_display.php
@@ -63,11 +63,9 @@
 
     <body>
     <ul>
-        <li><a href="../home.html">Home</a></li>
-        <li><a href="../profile.html">Profile</a></li>
+        <li><a href="../../static_web_views/home.html">Home</a></li>
+        <li><a href="../views/profile.php">Profile</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="../tables/department_table.php">Back to Table</a></li>
-        <li><a href="../searches/department_search.php">Back to Search</a></li>
     </ul>
     <h1>Advanced Search Display</h1>
 
