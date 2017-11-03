cab5ceb8fadfa958b36a2513604ce9327d54df33
mmcken3 <mmcken3@g.clemson.edu>
2017-11-03 09:25:13 -0400

    more data creation scripts, only 2 remaining

---
+++ data/create_inventory.py
@@ -0,0 +1,51 @@
+from random import *
+
+download_dir = "inventory.csv" #where you want the file to be downloaded to 
+
+csv = open(download_dir, "w") 
+#"w" indicates that you're writing strings to the file
+
+inv_type_list = ["fencing", "concrete blocks", "tile", "marble", "2x4 lumber", "7x7 lumber",
+            "dry wall nails", "lumber nails", "roofing nails", "cement nails", "nail gun", 
+            "drill", "roofing", "plastic siding", "steel beams", "wooden siding"]
+
+category_list = ["nails", "steel", "flooring", "lumber", "tile", "hand tools", "power tools",
+					"trucks", "dirt", "cement", "doors", "windows", "roofing"]
+
+inv_supplier_list = ["Lowes", "Home Depot", "Concrete Supply Co", "Gateway Supply Co",
+						"Builders First Source", "Blue Linx Corp", "HD Supply White Cap", "ABC Supply Co",
+						"Pro Build Holdings", "Roofing Supply Group", "L&W Supply", "84 Lumber",
+						"Beacon Roofing Supply", "Allied Building Products", "Yella Wood"]
+
+inv_location_name_list = ["Douthit Hills", "HWY 123 - HWY 76", "River St. Downtown", "Pleasantburg Shopping"
+						"Furman Student Center", "Epicentre", "Mint Street Parking Deck", "Lincoln Heights",
+						"Hemming Plaza", "N Georgia - Gator Bowl Blvd", "Bank of America FL", "Georgia Tech Student Center",
+						"Administrative Services Clemson", "Jaxonville City Hall"]
+
+product_type_list = ["nails", "steel", "flooring", "lumber", "tile", "hand tools", "power tools",
+					"trucks", "dirt", "cement", "doors", "windows", "roofing"]
+
+columnTitleRow = "inventory_type, category, number_of, inv_supplier_name, inv_location_name\n"
+#csv.write(columnTitleRow)
+
+index = 1
+while index < 400:
+    toWrite = "" + str(index) + ", "
+
+    inv_type_num = randint(0, len(inv_type_list) - 1)
+    toWrite = toWrite + "'" + inv_type_list[inv_type_num] + " - " + str(randint(0, 400)) + "', "
+
+    category_num = randint(0, len(category_list) - 1)
+    toWrite = toWrite + "'" + category_list[category_num] + "', "
+
+    number_of = randint(0, 10000)
+    toWrite = toWrite + "'" + str(number_of) + "', "
+
+    supplier_name_num = randint(0, len(inv_supplier_list) - 1)
+    toWrite = toWrite + "'" + inv_supplier_list[supplier_name_num] + "', "
+
+    location_name_num = randint(0, len(inv_location_name_list) - 1)
+    toWrite = toWrite + "'" + inv_location_name_list[location_name_num] + " - " + str(randint(0, 100)) + "'\n"
+
+    csv.write(toWrite)
+    index = index + 1
---
+++ data/create_suppliers.py
@@ -0,0 +1,51 @@
+from random import *
+
+download_dir = "suppiers.csv" #where you want the file to be downloaded to 
+
+csv = open(download_dir, "w") 
+#"w" indicates that you're writing strings to the file
+
+supplier_name_list = ["Lowes", "Home Depot", "Concrete Supply Co", "Gateway Supply Co",
+						"Builders First Source", "Blue Linx Corp", "HD Supply White Cap", "ABC Supply Co",
+						"Pro Build Holdings", "Roofing Supply Group", "L&W Supply", "84 Lumber",
+						"Beacon Roofing Supply", "Allied Building Products", "Yella Wood"]
+
+supplier_location_name_list = ["Douthit Hills", "HWY 123 - HWY 76", "River St. Downtown", "Pleasantburg Shopping"
+						"Furman Student Center", "Epicentre", "Mint Street Parking Deck", "Lincoln Heights",
+						"Hemming Plaza", "N Georgia - Gator Bowl Blvd", "Bank of America FL", "Georgia Tech Student Center",
+						"Administrative Services Clemson", "Jaxonville City Hall"]
+
+product_type_list = ["nails", "steel", "flooring", "lumber", "tile", "hand tools", "power tools",
+					"trucks", "dirt", "cement", "doors", "windows", "roofing"]
+
+first_name = ["Jack", "Alex", "Alexa", "Katie", "Catherine", "Kyle", "Ross", "Peter", "John", "Brett",
+			"Liz", "Nick", "Caitlin", "Lyla", "Colin", "Patrick", "Cameron", "Anna"]
+last_name = ["Smith", "Doe", "Simpson", "Williams", "Powell", "Watson", "Carr", "Brady", "Adams",
+			"Shoemaker", "Washington"]
+
+columnTitleRow = "supplier_name, supplier_location_name, product_type, contact_name, phone_number\n"
+#csv.write(columnTitleRow)
+
+index = 1
+for name in supplier_name_list:
+	toWrite = "" + str(index) + ", "
+		
+	toWrite = toWrite + "'" + name + "',"
+
+	location_name_num = randint(0, len(supplier_location_name_list) - 1)
+	toWrite = toWrite + "'" + supplier_location_name_list[location_name_num] + " - " + str(randint(0, 100)) + "', "
+
+	product_type_num = randint(0, len(product_type_list) - 1)
+	toWrite = toWrite + "'" + product_type_list[product_type_num] + "', "
+
+	first_name_num = randint(0, len(first_name) - 1)
+	last_name_num = randint(0, len(last_name) - 1)
+	toWrite = toWrite + "'" + first_name[first_name_num] + " " + last_name[last_name_num] + "', "
+
+	contact_num_1 = randint(0, 999)
+	contact_num_2 = randint(0, 999)
+	contact_num_3 = randint(0, 9999)
+	toWrite = toWrite + "'" + str(contact_num_1) + "-" + str(contact_num_2) + "-" + str(contact_num_3) + "'\n"
+
+	csv.write(toWrite)
+	index = index + 1
