0f750c7b7cb3dec7e33e970afdab4e98257c1bc6
mmcken3 <mmcken3@g.clemson.edu>
2017-11-28 20:54:54 -0500

    updated comments in data_manager.

--- src/data_manager/data_manager.php
+++ src/data_manager/data_manager.php
@@ -5,6 +5,9 @@
 ?>
 <?php
 
+// This function starts up a connection to the database with the credentials
+// and DB location specfied with the config.ini file. Update that as needed for
+// connection changes.
 function connect(){
     $config = parse_ini_file("config.ini");
     
@@ -24,9 +27,13 @@ function connect(){
     return $conn;
 }
 
+// This is the request manager function that is needed for updating databae. It will check to see if
+// the values of the row of data have been updated since the user last viewed it. If they have
+// been changed this will return false, if they have not then it returns true.
 function checkTime($current, $table, $id_val) {
-    $conn = connect();
+    $conn = connect(); // connect
 
+    // Gets the value of last updated from the most current state in the table.
     $sql = "SELECT last_updated FROM " . $table . " WHERE " . $table . "_id = " . $id_val;
     $result = $conn->query($sql);
 
@@ -38,13 +45,13 @@ function checkTime($current, $table, $id_val) {
         // Free result set
         mysqli_free_result($result);
     }
-    $current = substr($current, 1, -1);
+    $current = substr($current, 1, -1); // removes the ' from the last updated string
 
-    $format = "Y/m/d H:i:s";
-
+    // converts the strings for each time into unix timestamps
     $current = strtotime(explode(" ", $current)[1]);
     $found_time = strtotime(explode(" ", $found_time)[1]);
 
+    // returns false if the passed timestamps are old
     if ($found_time > $current){
         return false;
     }
@@ -53,6 +60,8 @@ function checkTime($current, $table, $id_val) {
     return true;
 }
 
+// This function will backup the database to the system by taking town a full sql dump of
+// the schema and data. 
 function backup() {
     $login_user =  $_SESSION["login"];
     $admin_rights = $_SESSION["admin"];
@@ -67,6 +76,11 @@ function backup() {
     exec("mysqldump --user=" . $username . " --password=" . $password . " --host=" . $servername . " " . $database . " > ./backup_database.sql");
 }
 
+// Saves the passed data to the database based off the passed in table, field names, and values.
+// Based on create it knows whether to update or fully insert new data. Along with that it pulls out
+// the last_updated value from the viewed row in order to call checkTime before running the save. If
+// checkTime is fasle then this will request the user to re check the data before saving. 
+// It only edits yourself as an employee unless you are an admin, and in that case you can edit anything.
 function save($table_name, $fields, $values, $create) {
     $login_user =  $_SESSION["login"];
     $admin_rights = $_SESSION["admin"];
@@ -79,12 +93,16 @@ function save($table_name, $fields, $values, $create) {
         echo "Please go back to view the updated values.";
         return;
     }
-    $conn = connect();
+    $conn = connect(); // connect
 
+    // removes the timestamps from the values because we want that to auto update itself on save.
     $fields = substr($fields, 0, strrpos($fields, ','));
     $values = substr($values, 0, strrpos($values, ','));
 
+    // inserts into the table, at the fields, with the values, and updates in duplicate case.
     $sql = "insert into " . $table_name . " (" . $fields . ") values (" . $values . ") on duplicate key update ";
+    
+    // rotates through the fields and values to set the on duplicate update part of the sql statement.
     if ($fields != "") {
         $peicesf = explode(",", $fields);
         $peicesv = explode(",", $values);
@@ -97,11 +115,14 @@ function save($table_name, $fields, $values, $create) {
             }
         }
     }
+    // runs check rules to make sure everything fits business rules.
     if (!checkRules($table_name, $peicesf, $peicesv)){
         echo "<p></p>";
         echo "<h4>Not saved!</h4>";
         return;
     }
+
+    // does not update the employee table unless the user is admin.
     if ($table_name != 'employee') {
         $result = $conn->query($sql);
         echo "Saved!";
@@ -120,7 +141,11 @@ function save($table_name, $fields, $values, $create) {
     $conn->close();
 }
 
+// This will check a few rules to ensure that data has been entered and that users are
+// following rules such as departments having managers, projects having locations,
+// and inventory having suppliers. 
 function checkRules($table_name, $fields, $values) {
+    // if its department, it checks to be sure manager value is there.
     if ($table_name == 'department') {
         $department_manager_name = $values[2];
         if ($department_manager_name == 'null'){
@@ -129,7 +154,7 @@ function checkRules($table_name, $fields, $values) {
         }
         return true;
     }
-    else if ($table_name == 'project') {
+    else if ($table_name == 'project') { // if project, ensure a location value is there.
         $project_location_name = $values[4];
         if ($project_location_name == 'null'){
             echo "<h4>Project's must have a location!</h4>";
@@ -137,7 +162,7 @@ function checkRules($table_name, $fields, $values) {
         }
         return true;
     }
-    else if ($table_name == 'inventory') {
+    else if ($table_name == 'inventory') { // if it is inventory, ensure a location and supplier are there.
         $inventory_location_name = $values[5];
         $inventory_supplier = $values[4];
         if ($inventory_location_name == 'null' || $inventory_supplier == 'null'){
@@ -151,12 +176,17 @@ function checkRules($table_name, $fields, $values) {
     }
 }
 
+// The delete function will take a table, and id number and then delete that row
+// from the table. It will only delete from the employee table if the logged in user
+// is an admin account.
 function delete($table_name, $table_id, $id_value){
     $login_user =  $_SESSION["login"];
     $admin = $_SESSION["admin"];
 
-    $conn = connect();
+    $conn = connect(); // connects.
 
+    // does not delete from employee unless the user is admin.
+    // deletes in table, the row with id_value.
     if ($table_name != 'employee') {
         $sql = "delete from " . $table_name . " where " . $table_id . "=" . $id_value;
         $result = $conn->query($sql);
@@ -174,12 +204,15 @@ function delete($table_name, $table_id, $id_value){
     $conn->close();
 }
 
+// This will get the next up id. It does this by getting the last ID, and then looks
+// for the next one in the count.
 function get_id($table, $id_type) {
     $login_user =  $_SESSION["login"];
     $admin_rights = $_SESSION["admin"];
 
-    $conn = connect();
+    $conn = connect(); // connects.
     
+    // Gets only the id number of the highest id number in table.
     $sql = "SELECT " . $id_type . " FROM " . $table . " ORDER BY " . $id_type . " desc limit 1;";
     $result = $conn->query($sql);
     
@@ -197,16 +230,21 @@ function get_id($table, $id_type) {
     $conn->close();
 }
 
+// This will search the database for a table, with the specified arguments passed in
+// that are comma delimted and then will follow the specified order in the the order
+// variable. The fields are limited in display if a user is not an admin.
 function search($table_name, $key, $args, $order){
     $login_user =  $_SESSION["login"];
     $admin_rights = $_SESSION["admin"];
 
-    $conn = connect();
+    $conn = connect(); // connects.
 
+    // Gets a list of column names from the passed table.
     $sql = "SHOW COLUMNS FROM " . $table_name;
     $result = $conn->query($sql);
     $column_array = array();
 
+    // Sets up a table and headers, showing admin and password info only to admins.
     echo "<table id='table'>";
     echo "<tr id='headers'>";
     while($row = $result->fetch_assoc()) {
@@ -233,23 +271,29 @@ function search($table_name, $key, $args, $order){
     }
     echo "</tr>";
 
+    // Seperates the arguments on commas to get each specifc part of data.
     $peices = explode(",", $args);
     $myArgs = "";
     for ($i = 0; $i < sizeof($peices) - 1; $i++){
+        // iterates through args building column_name = column_value AND ...
         $myArgs = $myArgs . $peices[0] . " AND ";
     }
     $myArgs = $myArgs . $peices[(sizeof($peices) - 1)];
 
+    // Builds a specific select statement based on the passed key, arguments, and order.
     $sql = "SELECT * FROM " . $table_name;
     if ($args != '') {
-        $sql = $sql . " " . $key . " " . $myArgs;
+        $sql = $sql . " " . $key . " " . $myArgs; // key is where.
     }
+
+    // Gets the order by value and column to order by if it is selected.
     if ($order != "") {
         $orderParts = explode(",", $order);
         $sql = $sql . " ORDER BY " . $orderParts[1] . " " . $orderParts[0];
     }
     $result = $conn->query($sql);
 
+    // Adds the results from the seach query to the table for display.
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             echo "<tr id='row'>";
